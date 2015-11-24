253b512a38167411ce42e71e26078e0ee3413e30
azeral <bondvsev@gmail.com>
2015-11-24 12:33:11 +0200

    Configure Spring MVC with annotations

--- src/main/java/net/bondar/web/configuration/AppConfig.java
+++ src/main/java/net/bondar/web/configuration/AppConfig.java
@@ -1,7 +1,36 @@
 package net.bondar.web.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
 /**
  * Created by Azeral on 24.11.2015.
  */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "net.bondar.web")
 public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+//    @Bean
+//    public MessageSource messageSource(){
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("message");
+//        return messageSource;
+//    }
 }
