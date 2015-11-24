9e8d8eace9ad31c26a50003099a5d42c0a4b74b7
azeral <bondvsev@gmail.com>
2015-11-24 11:32:13 +0200

    Initial commit

---
+++ src/main/java/net/bondar/web/controller/AppController.java
@@ -0,0 +1,7 @@
+package net.bondar.web.controller;
+
+/**
+ * Created by Azeral on 24.11.2015.
+ */
+public class AppController {
+}
---
+++ src/main/java/net/bondar/web/service/AppService.java
@@ -0,0 +1,7 @@
+package net.bondar.web.service;
+
+/**
+ * Created by Azeral on 24.11.2015.
+ */
+public interface AppService {
+}
