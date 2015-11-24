5d6135fbe7d9d972ae1416865cbdc28fd23da9f8
azeral <bondvsev@gmail.com>
2015-11-24 12:40:33 +0200

    Configure Initializer with annotations

--- src/main/java/net/bondar/web/configuration/AppInitializer.java
+++ src/main/java/net/bondar/web/configuration/AppInitializer.java
@@ -1,7 +1,44 @@
 package net.bondar.web.configuration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 /**
  * Created by Azeral on 24.11.2015.
  */
-public class AppInitializer {
+//public class AppInitializer implements WebApplicationInitializer{
+//
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//        context.register(AppConfig.class);
+//        context.setServletContext(servletContext);
+//
+//        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+//        servlet.setLoadOnStartup(1);
+//        servlet.addMapping("/");
+//    }
+//}
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
